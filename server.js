088a106a49719617bb6e67910cf7172ab7d9812c
jacob hoefer <jacob.hoefer@gmail.com>
2023-10-25 09:21:16 -0400

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const { engine } = require('express-handlebars');
+const session = require('express-session');
+
+
+// view routes
+
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.engine('.hbs', engine({ extended: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(session({
+    secret: 'tieflingBard',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/');
+app.use('/auth', user_routes);
+
+db.sync({ force: false })
+.then(() => {
+    app.listen(PORT, () => console.log(`happy surfing on port ${PORT}`));
+});
